92cc8caa7a7a370b3b53c446f6f8f0900456d7b7
Hubert <Hubert>
2022-03-19 13:35:26 +0100

    adding dynamic questions

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -4,12 +4,13 @@ import Answer from "./Answer";
 export default function Question() {
   const answerList = [];
   for (let i = 0; i < 4; i++) {
-    answerList.push(<Answer value={`Answer ${i}`} />);
+    answerList.push(<Answer value={props.data.incorrect_answers[i]} />);
   }
+  answerList.push(props.data.correct_answer);
 
   return (
     <div className="question">
-      <p className="question__text">Question</p>
+      <p className="question__text">{props.data.question}</p>
       <div className="question__answer-wrapper">{answerList}</div>
     </div>
   );
--- src/components/QuizzGame.jsx
+++ src/components/QuizzGame.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Question from "./Question";
+import data from "../data";
 
 export default function QuizzGame() {
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState(data);
 
-  useEffect(() => {
-    const dummyQuestion = [];
-    for (let i = 0; i < 5; i++) {
-      dummyQuestion.push(`Question ${i}`);
-    }
-    setQuestions(dummyQuestion);
-  }, []);
+  // useEffect(() => {
+  //   const dummyQuestion = [];
+  //   for (let i = 0; i < 5; i++) {
+  //     dummyQuestion.push(`Question ${i}`);
+  //   }
+  //   setQuestions(dummyQuestion);
+  // }, []);
 
   const questionList = questions.map((question) => {
-    return <Question />;
+    return <Question data={question} />;
   });
 
   return <div className="quizz-game">{questionList}</div>;
