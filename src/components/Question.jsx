456d428b62cb393551cc537c5188235b7bf5ba7d
Hubert <Hubert>
2022-03-28 20:47:08 +0200

    checking good answers and marking them finished

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -5,134 +5,76 @@ import { nanoid } from "nanoid";
 import he from "he";
 import { render } from "@testing-library/react";
 
+function shuffle(array) {
+  if (!array) return;
+
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
 export default function Question(props) {
   /*
     Push all wrong answers as <Answer/> componets in to array
     Adding corect answer also as <Answer/> componets 
     Shuffling array using  Durstenfeld's shuffle algorithm
   */
+
   const [selectedAnswer, setSelectedAnswer] = useState("");
-  const [answerList, setAnswerList] = useState([]);
+  // const [correctAnswer, setCorrectAnswer] = useState(props.correct);
 
+  let answersList;
+  let decodedQuestion = "";
+
+  function handleSelect(answerID) {
+    setSelectedAnswer(answerID);
+  }
+
+  /*correct | selected / endGame     "" | selected | correct | false
+      0         0           0         1
+    /  0         1           0                 1
+      1         0           0         1  
+      0         1           1                                     1
+      1         0           1                           1
+      1         1           1                           1
+ */
+
+  //   }
+  const [answers, setAnswers] = useState([]);
   useEffect(() => {
-    const answerOptions = [
-      ...props.data.incorrect_answers,
-      props.data.correct_answer,
-    ];
-
-    let answerObjects = answerOptions.map((answer) => {
-      let answerID = nanoid();
-      let cor = undefined;
-      if (props.endGame) {
-        if (answer === props.data.correct_answer) {
-          cor = true;
-        } else if (answer !== props.data.correct_answer) {
-          cor = false;
-        }
-      }
-
-      return {
-        value: answer,
-        correct: cor,
-        id: answerID,
-        selected: false,
-      };
-    });
-
-    if (!answerList) {
-      for (let i = answerObjects.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let temp = answerObjects[i];
-        answerObjects[i] = answerObjects[j];
-        answerObjects[j] = temp;
-      }
-    }
-    setAnswerList(answerObjects);
+    let shuffledAnswers = shuffle(props.answers);
+    setAnswers(shuffledAnswers);
   }, []);
 
-  let ComponentList = answerList.map((answer) => {
+  decodedQuestion = he.decode(props.text);
+  answersList = answers.map((answer) => {
     return (
-      <Answer
-        value={answer.value}
-        correct={answer.correct}
-        key={answer.id}
-        id={answer.id}
-        selected={answer.selected}
-        handleClick={() => handleSelect(answer.id)}
-      />
+      <div
+        key={answer}
+        className={`button question__answer ${
+          answer === props.correct && props.end ? "correct" : ""
+        } ${selectedAnswer === answer && !props.end ? "selected" : ""}  ${
+          props.end && answer !== props.correct && selectedAnswer === answer
+            ? "wrong"
+            : ""
+        }`}
+        onClick={() => handleSelect(answer)}
+      >
+        {answer}
+      </div>
     );
   });
 
-  let decodedQuestion = he.decode(props.data.question);
-
-  function handleSelect(answerID) {
-    // console.log(answerID);
-    setSelectedAnswer(answerID);
-    setAnswerList((prevAnswerList) => {
-      return prevAnswerList.map((prevAnswer) => {
-        return {
-          ...prevAnswer,
-          selected: answerID === prevAnswer.id ? !prevAnswer.selected : false,
-        };
-      });
-    });
-  }
-
-  //Only on First Run
-  // useEffect(() => {
-
-  // renderAnswers();
-  // }, []);
-
-  // rerenders answers when data
-  // useEffect(() => {
-  //   console.log("rerender");
-  //   renderAnswers();
-  // }, [answerList]);
-
-  // function checkAnswers() {
-  //   console.log("checking answers");
-  //   /*
-  //     map through answerList
-  //     if answerList.value === props.data.correct_answer && answerList.selected
-  //       change that answer "correct" value to true
-  //     else if answerList.selected && answer
-
-  //     selected | answer is true | color    |  style
-  //         1             1         green       --correct
-  //         1             0         red         --wrong
-  //         0             1         green       --correct
-  //         0             0         default
-
-  //   */
-
-  //   //Changes
-  //   setAnswerList((prevAnswerList) => {
-  //     // console.log(prevAnswerList);
-  //     return prevAnswerList.map((prevAnswer) => {
-  //       return {
-  //         ...prevAnswer,
-  //         correct:
-  //           prevAnswer.value === props.data.correct_answer ? true : false,
-  //       };
-  //     });
-  //   });
-  //   console.log("changed answer list: " + answerList);
-  // }
-  //
-  // if (props.endGame) {
-  //   checkAnswers();
-  // }
-
-  // useEffect(() => {
-  //   console.log("checking if game has ended");
-  //   if (props.endGame) checkAnswers();
-  // }, [props.endGame]);
+  console.log(selectedAnswer);
 
   return (
     <div className="question">
       <p className="question__text">{decodedQuestion}</p>
-      <div className="question__answer-wrapper">{ComponentList}</div>
+      <div className="question__answer-wrapper">{answersList}</div>
     </div>
   );
 }
--- src/components/QuizzGame.jsx
+++ src/components/QuizzGame.jsx
@@ -5,21 +5,43 @@ import data from "../data";
 import { nanoid } from "nanoid";
 
 export default function QuizzGame() {
-  const [questions, setQuestions] = useState(data);
+  const [questions, setQuestions] = useState([]);
   const [endGame, setEndGame] = useState(false);
+
+  useEffect(() => {
+    setQuestions(data);
+  }, []);
+
+  console.log(questions);
+
+  let questionElements = [];
+  questionElements =
+    questions !== []
+      ? questions.map((question, i) => {
+          let answers = [
+            ...question.incorrect_answers,
+            question.correct_answer,
+          ];
+          console.log(answers);
+          return (
+            <Question
+              text={question.question}
+              correct={question.correct_answer}
+              end={endGame}
+              answers={answers}
+              key={question.question}
+            />
+          );
+        })
+      : [];
 
   function turnEndGame() {
     console.log("toggling end game");
     setEndGame((prevEndGame) => !prevEndGame);
   }
-
-  const questionList = questions.map((question) => {
-    return <Question data={question} endGame={endGame} key={nanoid()} />;
-  });
-
   return (
     <div className="quizz-game">
-      {questionList}
+      {questionElements}
       <button className="button quizz-game__button" onClick={turnEndGame}>
         Check Answers
       </button>
